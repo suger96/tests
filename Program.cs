ca7fcc93f2bf285e371b91881e709614f5594a40
suger96 <alimowffaq@gmail.com>
2025-11-05 15:08:51 +0300

    inital work

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HR;
+
+internal abstract class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int number = 10;
+        var bytes = BitConverter.GetBytes(number);
+        foreach (var item in bytes)
+        {
+            var binary = Convert.ToString(item, 2).PadLeft(8, '0');
+            Console.WriteLine(binary);
+            
+        }
+
+        Console.ReadKey();
+    }
+}
